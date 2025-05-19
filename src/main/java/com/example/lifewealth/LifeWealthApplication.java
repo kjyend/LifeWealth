0ee074a0e1c5e778d8f7670301ece16a0698dd26
junyoung <adad568@naver.com>
2025-05-19 15:13:03 +0900

    Init: Init Project

---
+++ src/main/java/com/example/lifewealth/LifeWealthApplication.java
@@ -0,0 +1,13 @@
+package com.example.lifewealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeWealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeWealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lifewealth/LifeWealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lifewealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeWealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
