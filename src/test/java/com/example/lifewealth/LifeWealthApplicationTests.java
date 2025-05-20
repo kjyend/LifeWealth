6e72456fd272526867b8bcc5470ca3ea12220204
junyoung <adad568@naver.com>
2025-05-20 15:13:25 +0900

    Test: 복리 계산기 생성 테스트 생성

--- src/test/java/com/example/lifewealth/LifeWealthApplicationTests.java
+++ src/test/java/com/example/lifewealth/LifeWealthApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.lifewealth;
 
+import com.example.lifewealth.calculator.CompoundInterestCalculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,8 @@ class LifeWealthApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void createCompoundInterestCalculator(){
+        CompoundInterestCalculator calculator =new CompoundInterestCalculator();
+    }
 }
