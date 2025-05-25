a4508d55af63e66770a4bd67dd6fbac6c15e300d
junyoung <adad568@naver.com>
2025-05-25 20:38:40 +0900

    Test: 원금 1000, 년도 1, 이자율 0 일때 결과 1000 테스트 추가

--- src/test/java/com/example/lifewealth/LifeWealthApplicationTests.java
+++ src/test/java/com/example/lifewealth/LifeWealthApplicationTests.java
@@ -45,4 +45,13 @@ class LifeWealthApplicationTests {
         calculator.applyInterestRate(0);
         assertEquals(calculator.getCompoundResult(),1000);
     }
+
+    @Test
+    void calculateCompoundInterest_input_principal_1000_year_1_interestRate_0_return_1000(){
+        Calculator calculator = new Calculator();
+        calculator.applyPrincipal(1000);
+        calculator.applyYears(1);
+        calculator.applyInterestRate(0);
+        assertEquals(calculator.getCompoundResult(),1000);
+    }
 }
