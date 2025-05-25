18d768f1527e13636fb07be1b5394c4291d44582
junyoung <adad568@naver.com>
2025-05-25 20:28:45 +0900

    Refactor(Calculator): 원금 저장 및 복리 계산 조회 기능 구현

--- src/main/java/com/example/lifewealth/calculator/Calculator.java
+++ src/main/java/com/example/lifewealth/calculator/Calculator.java
@@ -1,12 +1,15 @@
 package com.example.lifewealth.calculator;
 
 public class Calculator {
-    public void calculateCompoundInterest(int principal) {
 
+    private int principal;
+
+    public void calculateCompoundInterest(int principal) {
+        this.principal=principal;
     }
 
     public int getCompoundResult() {
-        return 0;
+        return principal;
     }
 
     public void applyYears(int years) {
