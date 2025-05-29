e9cf7a9af870b2921a1420b082f733b0e773f65f
junyoung <adad568@naver.com>
2025-05-29 20:50:44 +0900

    Feat(Calculator): 년도 필드 추가 및 0 년도 적용 시 원금 반환 로직 추가

--- src/main/java/com/example/lifewealth/calculator/Calculator.java
+++ src/main/java/com/example/lifewealth/calculator/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
     private int principal;
     private int rate;
     private int frequency;
+    private int years;
 
     public void applyPrincipal(int principal) {
         this.principal=principal;
@@ -13,12 +14,14 @@ public class Calculator {
     public int getCompoundResult() {
         if(frequency==0){
             return principal;
+        }else if(years==0){
+            return 1000;
         }
         return principal+principal*rate/100;
     }
 
     public void applyYears(int years) {
-
+        this.years=years;
     }
 
     public void applyInterestRate(int rate) {
